81ca5360f0b51e4194b1efae3bf1b2a1d80fc6bd
Jonathan1600 <70250506+Jonathan1600@users.noreply.github.com>
2021-02-19 12:19:20 -0700

    broken animation

--- src/App.js
+++ src/App.js
@@ -1,26 +1,76 @@
 import React, { useState, useEffect } from "react";
 import "./App.css";
+import axios from "axios";
+import Character from "./components/Character";
+import styled, { keyframes } from "styled-components";
 
 const App = () => {
+  const [characters, setCharacters] = useState(null);
+  const [display, setDisplay] = useState(null);
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
   useEffect(() => {
     axios
-      .get("url")
+      .get("https://swapi.dev/api/people")
       .then((res) => {
-        console.log(res.data);
+        setCharacters(res.data);
       })
       .catch((err) => {
         console.log(err);
       });
   }, []);
+
+  const changeDisplay = (id) => {
+    if (display == id) {
+      setDisplay(null);
+    } else {
+      setDisplay(id);
+    }
+  };
+
+  const MainBox = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-wrap: wrap;
+    flex-direction: column;
+    background-image: ;
+  `;
+  const Holder = styled.div`
+    box-sizing: border-box;
+    background: rgba(18, 36, 57, 0.67);
+    border-radius: 5px;
+    padding: 50px;
+    display: flex;
+    flex-direction: column;
+    flex-wrap: wrap;
+    align-items: center;
+    justify-content: center;
+    width: 80vw;
+  `;
   // Fetch characters from the API in an effect hook. Remember, anytime you have a
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
-
+  if (!characters) return <h3>Loading...</h3>;
   return (
-    <div className="App">
-      <h1 className="Header">Characters</h1>
+    <div className="bg">
+      <MainBox className="App">
+        <h1 className="Header">Characters</h1>
+        <Holder>
+          {characters.map((character, i) => {
+            return (
+              <Character
+                action={changeDisplay}
+                displayId={i}
+                className="info"
+                display={display}
+                character={character}
+              />
+            );
+          })}
+        </Holder>
+        ;
+      </MainBox>
     </div>
   );
 };
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,58 @@
+import React, { useState } from "react";
+import styled, { keyframes } from "styled-components";
 // Write your Character component here
+const kf = keyframes`
+  100% {
+    opacity: 1;
+  }
+`;
+const Name = styled.div`
+  box-sizing: border-box;
+  border-radius: 3px;
+  width: 60vw;
+  background-color: #1b262c;
+  color: white;
+  margin: 0px;
+  padding: 20px;
+  border: 1px solid #3282b8;
+`;
+const Info = styled.div`
+  margin: 0px;
+  padding: 10px;
+  box-sizing: border-box;
+  opacity: 0%;
+  justify-content: space-between;
+  border-radius: 3px;
+  background-color: #0f4c75;
+  width: 60vw;
+  color: white;
+  display: ${(props) => (props.display === props.displayId ? "block" : "none")};
+
+  /* &:hover {
+    transform: scale(1.3);
+    padding: 5%;
+    border-radius: 3px;
+    transition: all 0.2s ease-in-out; 
+  }*/
+  transition: all 0.2s ease-in-out;
+  animation: ${kf} 0.5s ease-in-out forwards;
+`;
+
+export default function Character(props) {
+  const { character, displayId, display, action } = props;
+
+  return (
+    <div>
+      <Name onClick={() => action(displayId)}>{character.name}</Name>
+      <Info display={display} displayId={displayId}>
+        <p>Gender: {character.gender}</p>
+        <p>Height: {character.height}</p>
+        <p>Mass: {character.mass}</p>
+        <p>Birt Year: {character.birth_year}</p>
+        <p>Eye Color: {character.eye_color}</p>
+        <p>Hair Color: {character.hair_color}</p>
+        <p>Skin Color: {character.skin_color}</p>
+      </Info>
+    </div>
+  );
+}
