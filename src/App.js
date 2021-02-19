4c84b90b646a94e189718cad85b66f0531235589
Jonathan1600 <70250506+Jonathan1600@users.noreply.github.com>
2021-02-19 10:35:58 -0700

    test

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
-import React from 'react';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
-
-  // Fetch characters from the API in an effect hook. Remember, anytime you have a 
+  useEffect(() => {
+    axios
+      .get("url")
+      .then((res) => {
+        console.log(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+  // Fetch characters from the API in an effect hook. Remember, anytime you have a
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
 
@@ -14,6 +23,6 @@ const App = () => {
       <h1 className="Header">Characters</h1>
     </div>
   );
-}
+};
 
 export default App;
